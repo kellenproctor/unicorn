22a6e9a11044cb7866bd682eb551b91d2efedf40
Kellen Proctor <kellenproctor@gmail.com>
2018-09-02 17:23:52 -0700

    Create sign in and add to header (also Google Icon!)

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,16 @@ import React from 'react'
 import { Link } from 'gatsby'
 import styled from 'styled-components'
 import PropTypes from 'prop-types'
-import Container from '../styledComponents/layout'
+import { default as BaseContainerStyles } from '../styledComponents/layout'
+
+import SignIn from './SignIn/index'
+import GoogleIcon from './icons/Google'
+
+const Container = styled(BaseContainerStyles)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
 
 const HeaderContainer = styled.header`
   ${props => props.background};
@@ -18,14 +27,23 @@ const StyledLink = styled(Link)`
   text-decoration: none;
 `
 
+const StyledGoogleIcon = styled(GoogleIcon)`
+  margin-right: 5px;
+`
+
 const BACKGROUND = 'background-color: #20232a'
 
-const Header = ({ background, title }) => (
+const Header = ({ background, title, isAuthed, signIn, signOut }) => (
   <HeaderContainer background={background}>
     <Container>
       <Heading1>
         <StyledLink to="/">{title}</StyledLink>
       </Heading1>
+      <SignIn
+        onClick={() => (isAuthed ? signOut() : signIn('google'))}
+        icon={isAuthed ? null : <StyledGoogleIcon />}
+        text={isAuthed ? 'Sign Out' : 'Sign in with Google'}
+      />
     </Container>
   </HeaderContainer>
 )
@@ -37,7 +55,10 @@ Header.defaultProps = {
 
 Header.propTypes = {
   background: PropTypes.string,
+  signIn: PropTypes.func.isRequired,
+  signOut: PropTypes.func.isRequired,
   title: PropTypes.string,
+  isAuthed: PropTypes.bool,
 }
 
 export default Header
