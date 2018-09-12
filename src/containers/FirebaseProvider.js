11019ccfc65e4c838d5492ee6c18c763957ab4f6
Kellen Proctor <kellenproctor@gmail.com>
2018-09-11 20:48:44 -0700

    update Firebase to FirebaseContext

--- src/containers/FirebaseProvider.js
+++ src/containers/FirebaseProvider.js
@@ -2,13 +2,13 @@
 import React from 'react'
 
 import firebase from '../services/firebase'
-import Firebase from './FirebaseContext'
+import FirebaseContext from './FirebaseContext'
 
 const FirebaseProvider = ({ element }) => {
   return (
-    <Firebase.Provider firebase_browser={firebase.auth}>
+    <FirebaseContext.Provider firebase_browser={firebase.auth}>
       {element}
-    </Firebase.Provider>
+    </FirebaseContext.Provider>
   )
 }
 
