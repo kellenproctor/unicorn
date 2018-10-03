985d61b5e363c0be79523ed30f69756a38978334
Kellen Proctor <kellenproctor@gmail.com>
2018-10-02 20:02:26 -0700

    favicons and service workers

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,23 +4,34 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
-    },
-    'gatsby-plugin-offline',
     'gatsby-plugin-styled-components',
     {
       resolve: `gatsby-plugin-create-client-paths`,
       options: { prefixes: [`/poll/*`] },
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: 'Polling App',
+        short_name: 'Polling App',
+        start_url: '/',
+        background_color: '#08AeeA',
+        theme_color: '#2AF598',
+        display: 'minimal-ui',
+        icons: [
+          {
+            src: `/favicons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+      },
+    },
+    'gatsby-plugin-offline',
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -2,7 +2,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import { StaticQuery, graphql } from 'gatsby'
+import { StaticQuery, graphql, withPrefix } from 'gatsby'
 import styled from 'styled-components'
 
 import FirebaseContext from '../containers/FirebaseContext'
@@ -24,9 +24,28 @@ const Layout = ({ data, children, props }) => (
             <Helmet
               title={data.site.siteMetadata.title}
               meta={[
-                { name: 'description', content: 'Sample' },
-                { name: 'keywords', content: 'sample, something' },
+                { name: 'description', content: 'Create polls for stuff and things' },
+                { name: 'keywords', content: 'polling, rating' },
+                { name: 'msapplication-TileColor', content: '#08AEEA'},
+                { name: 'theme-color', content: '#2AF598'}
               ]}>
+              <link
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href={withPrefix('/favicons/apple-touch-icon.png')}
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href={withPrefix('/favicons/favicon-32x32.png')}
+              />
+              <link
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href={withPrefix('/favicons/favicon-16x16.png')}
+              />
               <html lang="en" />
             </Helmet>
             <Header
